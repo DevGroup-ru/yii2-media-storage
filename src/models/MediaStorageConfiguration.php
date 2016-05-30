8822082e9543ec74b40d9ef15911ef3011d30057
Ivan Salnikov <salnikov@devgroup.ru>
2016-05-30 11:46:39 +0300

    small copy

--- src/models/MediaStorageConfiguration.php
+++ src/models/MediaStorageConfiguration.php
@@ -4,9 +4,97 @@
 namespace DevGroup\MediaStorage\models;
 
 use DevGroup\ExtensionsManager\models\BaseConfigurationModel;
+use Yii;
+use yii\helpers\ArrayHelper;
 
 class MediaStorageConfiguration extends BaseConfigurationModel
 {
+
+    public $defaultThumbnailSize;
+    public $noImageSrc;
+    public $thumbnailsDirectory;
+    public $useWatermark;
+    public $watermarkDirectory;
+    public $defaultComponents = [];
+    public $defaultComponent;
+    public $components = [];
+
+    public function rules()
+    {
+        return [
+            [['noImageSrc', 'defaultThumbnailSize', 'thumbnailsDirectory', 'watermarkDirectory'], 'string'],
+            ['useWatermark', 'boolean'],
+            [['components', 'defaultComponents'], 'isArray'],
+        ];
+    }
+
+    public function isArray($attribute, $params)
+    {
+        if (!is_array($this->$attribute)) {
+            $this->addError($attribute, "The $attribute must be array");
+        }
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'defaultThumbnailSize' => Yii::t('app', 'Default thumbnail size'),
+            'noImageSrc' => Yii::t('app', 'No image src'),
+            'thumbnailsDirectory' => Yii::t('app', 'Thumbnails directory'),
+            'useWatermark' => Yii::t('app', 'Use watermark'),
+            'watermarkDirectory' => Yii::t('app', 'Watermark directory'),
+            'components' => Yii::t('app', 'Components'),
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function defaultValues()
+    {
+        $module = Yii::$app->getModule('image');
+        $attributes = array_keys($this->getAttributes());
+        foreach ($attributes as $attribute) {
+            $this->{$attribute} = $module->{$attribute};
+        }
+        foreach ($this->defaultComponents as $name => $values) {
+            $this->defaultComponents[$name]['name'] = $name;
+        }
+    }
+
+    public function getAttributesForStateSaving()
+    {
+        $attributes = $this->getAttributes();
+        if (isset($attributes['components'])) {
+            foreach ($attributes['components'] as $name => $component) {
+                if (isset($component['necessary']) && isset($component['necessary']['active']) && $component['necessary']['active'] == false) {
+                    unset($attributes['components'][$name]);
+                }
+            }
+        }
+        if (isset($attributes['defaultComponents'])) {
+            foreach ($attributes['defaultComponents'] as $component) {
+                if (isset($component['necessary']) && isset($component['necessary']['active']) && $component['necessary']['active'] == true && !isset($attributes['components'][$component['name']])) {
+                    $newData = $component;
+                    unset($newData['name']);
+                    $attributes['components'][$component['name']] = $newData;
+                }
+            }
+            unset($attributes['defaultComponents']);
+        }
+        return $attributes;
+    }
+
+    /**
+     * Returns array of key=>values for configuration.
+     * @return mixed
+     */
+    public function keyValueAttributes()
+    {
+        return [];
+    }
+
+
     /**
      * Returns array of module configuration that should be stored in application config.
      * Array should be ready to merge in app config.
@@ -49,6 +137,23 @@ class MediaStorageConfiguration extends BaseConfigurationModel
      */
     public function commonApplicationAttributes()
     {
+        $attributes = $this->getAttributesForStateSaving();
+        $components = [];
+        foreach ($this->components as $name => $component) {
+            $necessary = ArrayHelper::getValue($component, 'necessary', []);
+            $unnecessary = ArrayHelper::getValue($component, 'unnecessary', []);
+            $active = ArrayHelper::remove($necessary, 'active', false);
+            ArrayHelper::remove($necessary, 'srcAdapter');
+            if ($active === true || $active === '1' || $name == 'fs') {
+                foreach ($unnecessary as $confName => $confVal) {
+                    if ($confVal === '') {
+                        ArrayHelper::remove($unnecessary, $confName);
+                    }
+                }
+                $components[$name] = ArrayHelper::merge($necessary, $unnecessary);
+            }
+        }
+
         return [
             'components' => [
                 'protectedFilesystem' => [
@@ -58,6 +163,10 @@ class MediaStorageConfiguration extends BaseConfigurationModel
                 'urlManager' => [
                     'excludeRoutes' => ['media/file/send', 'media/file/xsend'],
                 ],
+                $components,
+            ],
+            'modules' => [
+                'image' => $attributes,
             ],
         ];
     }
--- src/views/_configuration.php
+++ src/views/_configuration.php
@@ -1,7 +1,98 @@
 <?php
-/**
- * @var $this \yii\web\View
- */
+
+use yii\bootstrap\Tabs;
+
+$activeComponents = [];
+foreach (array_keys($model->components) as $value) {
+    $activeComponents[$value] = $value;
+}
+?>
+
+<div id="fs-config">
+    <div class="col-md-6 col-sm-12">
+
+        <div class="clearfix"></div>
+        <h2><?= Yii::t('app', 'Active Components') ?></h2>
+        <?php
+        foreach ($model['components'] as $componentName => $componentConf) {
+
+            $necessaryContent = "";
+            foreach ($componentConf['necessary'] as $necessaryConfName => $necessaryConfVal) {
+                $content = $form->field(
+                    $model,
+                    "components[{$componentName}][necessary][{$necessaryConfName}]"
+                )->label(
+                    $necessaryConfName
+                );
+                if (is_bool($necessaryConfVal) === true || $necessaryConfName === 'active') {
+                    $content = $content->widget(\kartik\widgets\SwitchInput::className());
+                }
+                $necessaryContent .= $content;
+            }
+            $unnecessaryContent = '';
+            foreach ($componentConf['unnecessary'] as $unnecessaryConfName => $unnecessaryConfVal) {
+                $unnecessaryContent .= $form->field(
+                    $model,
+                    "components[{$componentName}][unnecessary][{$unnecessaryConfName}]"
+                )->label(
+                    $unnecessaryConfName
+                );
+            }
+            echo Tabs::widget(
+                [
+                    'items' => [
+                        ['label' => Yii::t('app', 'necessary'), 'content' => $necessaryContent],
+                        ['label' => Yii::t('app', 'unnecessary'), 'content' => $unnecessaryContent],
+                    ],
+                ]
+            );
+
+        }
+        ?>
 
 
-echo $form->field($model, 'flysystem');
+    </div>
+    <div class="col-md-6 col-sm-12">
+        <h2><?= Yii::t('app', 'Add new component') ?></h2>
+        <?php
+        foreach ($model['defaultComponents'] as $componentName => $componentConf) {
+
+            $necessaryContent = $form->field(
+                $model,
+                "defaultComponents[{$componentName}][name]"
+            )->label(
+                'name'
+            );
+            foreach ($componentConf['necessary'] as $necessaryConfName => $necessaryConfVal) {
+                $content = $form->field(
+                    $model,
+                    "defaultComponents[{$componentName}][necessary][{$necessaryConfName}]"
+                )->label(
+                    $necessaryConfName
+                );
+                if (is_bool($necessaryConfVal) === true || $necessaryConfName === 'active') {
+                    $content = $content->widget(\kartik\widgets\SwitchInput::className());
+                }
+                $necessaryContent .= $content;
+            }
+            $unnecessaryContent = '';
+            foreach ($componentConf['unnecessary'] as $unnecessaryConfName => $unnecessaryConfVal) {
+                $unnecessaryContent .= $form->field(
+                    $model,
+                    "defaultComponents[{$componentName}][unnecessary][{$unnecessaryConfName}]"
+                )->label(
+                    $unnecessaryConfName
+                );
+            }
+            echo Tabs::widget(
+                [
+                    'items' => [
+                        ['label' => Yii::t('app', 'necessary'), 'content' => $necessaryContent],
+                        ['label' => Yii::t('app', 'unnecessary'), 'content' => $unnecessaryContent],
+                    ],
+                ]
+            );
+            
+        }
+        ?>
+    </div>
