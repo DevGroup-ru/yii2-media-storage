a606eead031ca2b32f0c3cd08107c551dabdb8f7
Ivan Salnikov <salnikov@devgroup.ru>
2016-05-31 14:48:13 +0300

    imprs

--- src/views/_configuration.php
+++ src/views/_configuration.php
@@ -5,15 +5,29 @@ use unclead\widgets\MultipleInput;
 use unclead\widgets\MultipleInputColumn;
 use yii\bootstrap\Html;
 use yii\bootstrap\Tabs;
+use yii\web\JqueryAsset;
+use yii\web\JsExpression;
 
 $activeComponents = [];
 
-echo $form->field($model, 'activeFS')->widget(
+echo $form->field(
+    $model,
+    'activeFS',
+    [
+        'horizontalCssClasses' => [
+            'label' => 'col-sm-12',
+            'offset' => '',
+            'wrapper' => 'col-sm-12',
+            'error' => '',
+            'hint' => '',
+        ],
+    ]
+)->widget(
     MultipleInput::className(),
     [
         'min' => 0,
         'allowEmptyList' => true,
-        'rowOptions' => function($model) {
+        'rowOptions' => function ($model) {
             $options = [];
             if ($model['priority'] > 1) {
                 $options['class'] = 'danger';
@@ -22,13 +36,13 @@ echo $form->field($model, 'activeFS')->widget(
         },
         'columns' => [
             [
-                'name'  => 'user_id',
-                'type'  => MultipleInputColumn::TYPE_DROPDOWN,
+                'name' => 'class',
+                'type' => MultipleInputColumn::TYPE_DROPDOWN,
                 'enableError' => true,
-                'title' => 'User',
-                'defaultValue' => 33,
-                /* it can be an anonymous function
-                'items' => function($data) {
+                'title' => 'FS',
+                //                'defaultValue' => 33,
+
+                'items' => function ($data) {
                     return [
                         31 => 'item 31',
                         32 => 'item 32',
@@ -37,57 +51,32 @@ echo $form->field($model, 'activeFS')->widget(
                         35 => 'item 35',
                         36 => 'item 36',
                     ];
-                }
-                */
-                'items' =>  [
-                    31 => 'item 31',
-                    32 => 'item 32',
-                    33 => 'item 33',
-                    34 => 'item 34',
-                    35 => 'item 35',
-                    36 => 'item 36',
-                ]
-            ],
-            [
-                'name'  => 'day',
-                'type'  => DatePicker::className(),
-                'title' => 'Day',
-                'value' => function($data) {
-                    return $data['day'];
                 },
-                'items' => [
-                    '0' => 'Saturday',
-                    '1' => 'Monday'
-                ],
                 'options' => [
-                    'pluginOptions' => [
-                        'format' => 'dd.mm.yyyy',
-                        'todayHighlight' => true
-                    ]
+                    'onchange' => new JsExpression(
+                        'var $parentRow = $(this).closest(\'.multiple-input-list__item\');
+$parentRow.next(\'.jsable-row\').remove();
+var template = \'<tr class="jsable-row"><td colspan="{{cols}}">{{configInputs}}</td></tr>\';
+$(template).insertAfter($parentRow);
+'
+                    ),
                 ],
-                'headerOptions' => [
-                    'style' => 'width: 250px;',
-                    'class' => 'day-css-class'
-                ]
             ],
             [
-                'name'  => 'priority',
-                'title' => 'Priority',
-                'defaultValue' => 1,
+                'name' => 'name',
+                'title' => 'name',
+                'defaultValue' => 'fs',
                 'enableError' => true,
-                'options' => [
-                    'class' => 'input-priority'
-                ]
             ],
             [
-                'name'  => 'comment',
-                'type'  => MultipleInputColumn::TYPE_STATIC,
-                'value' => function($data) {
+                'name' => 'comment',
+                'type' => MultipleInputColumn::TYPE_STATIC,
+                'value' => function ($data) {
                     return Html::tag('span', 'static content', ['class' => 'label label-info']);
                 },
                 'headerOptions' => [
                     'style' => 'width: 70px;',
-                ]
+                ],
             ],
             [
                 'type' => MultipleInputColumn::TYPE_CHECKBOX_LIST,
@@ -99,14 +88,14 @@ echo $form->field($model, 'activeFS')->widget(
                     1 => 'Test 1',
                     2 => 'Test 2',
                     3 => 'Test 3',
-                    4 => 'Test 4'
+                    4 => 'Test 4',
                 ],
                 'options' => [
                     // see checkboxList implementation in the BaseHtml helper for getting more detail
-                    'unselect' => 2
-                ]
-            ]
-        ]
+                    'unselect' => 2,
+                ],
+            ],
+        ],
     ]
 );
 
